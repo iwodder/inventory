095161369d3297895e05f4ed5dfd1bdc756708b0
iwodder <42330235+birdiedude@users.noreply.github.com>
2021-12-18 06:01:09 -0600

    Add Location value type

---
+++ src/main/java/com/wodder/inventory/domain/entities/Location.java
@@ -0,0 +1,44 @@
+package com.wodder.inventory.domain.entities;
+
+public class Location {
+	private static final String DEFAULT_VALUE = "unassigned";
+	private String name;
+
+	public Location(Location that) {
+		this(that.getName());
+	}
+
+	public Location(String name) {
+		setName(name);
+	}
+
+	public Location() {
+		this(DEFAULT_VALUE);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Location) {
+			return this.name.equals(((Location) obj).name);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	private void setName(String name) {
+		if (name != null && name.length() > 0) {
+			this.name = name;
+		} else {
+			this.name = DEFAULT_VALUE;
+		}
+	}
+}
